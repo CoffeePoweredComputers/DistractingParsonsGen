17b1e2461cc29589da4ce610223aa87810d84cc1
David H Smith IV <davidsmith@hamiltonfour.tech>
2022-03-28 18:35:54 -0500

    updated matcher with some more templates

--- server/transformations/Matcher.py
+++ server/transformations/Matcher.py
@@ -5,6 +5,46 @@ class Matcher(ast.NodeVisitor):
     def match_operation(self, node):
 
         match node:
+
+            #######################
+            # Collection Literals #
+            #######################
+
+            # {1, 2, 3, 4}
+            case :
+
+                return "collection_func", "set_literal"
+
+            # [1, 2, 3, 4]
+            case :
+
+                return "collection_func", "list_literal"
+
+            # s = {key1: val1, key2: val2}
+            case :
+
+                return "collection_func", "dict_literal"
+
+            # s = {1, 2, 3, 4}
+            case :
+
+                return "collection_func", "set_create"
+
+            # s = [1, 2, 3, 4]
+            case :
+
+                return "collection_func", "list_create"
+
+            # s = {key1: val1, key2: val2}
+            case :
+
+                return "collection_func", "dict_create"
+
+            ####################
+            # collection _func #
+            ####################
+
+            # result = lst.count(elem)
             case ast.Assign(
                     targets=[
                         ast.Name()],
@@ -17,6 +57,7 @@ class Matcher(ast.NodeVisitor):
 
                 return "collection_func", "count"
 
+            # lst.count(elem)
             case ast.Expr(
                     value=ast.Call(
                         func=ast.Attribute(
@@ -26,6 +67,7 @@ class Matcher(ast.NodeVisitor):
 
                 return "collection_func", "count"
 
+            # lst.append(elem)
             case ast.Expr(
                     value=ast.Call(
                         func=ast.Attribute(
@@ -36,6 +78,7 @@ class Matcher(ast.NodeVisitor):
 
                 return "collection_func", "append"
 
+            # lst.extend(other_seq)
             case ast.Expr(
                     value=ast.Call(
                         func=ast.Attribute(
@@ -45,26 +88,32 @@ class Matcher(ast.NodeVisitor):
                         args=[_])):
 
                 return "collection_func", "extend"
+
+            # 
+            case:
+
+            # 
+            case:
+
+            # 
+            case:
+
+            # 
+            case:
+
+            # 
+            case:
+
+            # 
+            case:
+
+            # 
+            case:
+
+            # 
+            case:
             
             case _:
-                print("no match")
+                return None
 
 
-
-#def main():
-#    v = Visitor()
-#    lines = [
-#        "z = x.count([1, 2, 3, {1, 2 ,3} ])",
-#        "test = x.count(5)",
-#        "x.count(5)",
-#        "lst.append(z)",
-#        "lst.append(y)",
-#        "lst.extend([1, 2, 3])"
-#    ]
-#    for line in lines:
-#        line = ast.parse(line.strip()).body[0]
-#        #print(ast.dump(line, indent=4))
-#        v.match_operation(line)
-#
-#if __name__ == "__main__":
-#    main()
